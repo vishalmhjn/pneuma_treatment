6bd08d8781395a69e6ccea1df40117365a40f257
Vishal <vishalmhjn3@gmail.com>
2022-11-12 13:07:35 +0100

    Update the processor

--- README.md
+++ README.md
@@ -6,8 +6,14 @@ Trajectory data collected from Drones over an urban area in Athens. For more inf
 <img src="media/pneuma_overview.gif" alt="drawing" width="400" align="center"/>
 </p>
 
-This repository provides an automated way to treat the noise and anomalies (in the form of unrealistic peaks) in the acceleration values of a number of vehicles in the pneuma dataset. The outputs are the processed time-series profiles of vehicle speeds and accelerations.
+This repository provides an automated way to treat the noise and anomalies (in the form of unrealistic peaks) in the acceleration values of a number of vehicles in the pneuma dataset. The algorithm uses a combination of low-pass filters (Savitzky-Golay filter, Gaussian filter) to remove the noise. A machine learning model (XGBoost) is used to reconstruct acceleration the time-series and detect the anomalies. If anomalies are detected, they are removed. The outputs are the processed time-series profiles of vehicle speeds and accelerations without high frequency noise and unrealistic acceleration peaks.
 
 # Code 
-1. Specify the path to the raw data in the [data_formatter.py](src/data_formatter.py) and run it to convert the files from wide to long format. 
-2. Specify the path to the output file from step-1, other paths (to save output data and plots) in the [processor.py](src/processor.py). Run this file to get the treated speed and acceleration.
+You should be in [src](src/) folder when running the codes.
+### Raw data to Long format
+1. Specify the path to the raw data from pNEUMA website in the [data_formatter.py](src/data_formatter.py) and run it to convert the files from wide to long format. A [sample](data/sample_data.csv) output is provided.
+### Treating noise and anomalies
+2. Specify the path to the output file from step-1, other paths (to save output data and plots) in the [processor.py](src/processor.py). Run this file to get the treated speed and acceleration time series.
+
+### Outputs
+3. With the default paths, the plots are saved in [plots](plots/) and data is saved in [data/derived](data/derived).
--- src/processor.py
+++ src/processor.py
@@ -1,30 +1,34 @@
-import random
-import math
 import utm
-
 import numpy as np
 import pandas as pd
-
+import matplotlib
+matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from glob import glob
 from tqdm import tqdm
 
-from scipy import stats
 from scipy.ndimage import gaussian_filter1d
 from scipy.signal import savgol_filter
-
 import xgboost as xg 
 
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import mean_squared_error as MSE 
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Ridge
 
 sns.set_style("whitegrid")
-import itertools
+
+SMALL_SIZE = 12
+MEDIUM_SIZE = 14
+BIGGER_SIZE = 16
+
+plt.rc('font', size=SMALL_SIZE)
+plt.rc('axes', titlesize=BIGGER_SIZE)
+plt.rc('axes', labelsize=BIGGER_SIZE)
+plt.rc('xtick', labelsize=BIGGER_SIZE)
+plt.rc('ytick', labelsize=BIGGER_SIZE)
+plt.rc('legend', fontsize=SMALL_SIZE)
+plt.rc('figure', titlesize=BIGGER_SIZE) 
 
 class PNEUMATrajectory:
 	def __init__(self, PATH):
@@ -35,10 +39,10 @@ class PNEUMATrajectory:
 		'''
 		self.df = pd.read_csv(self.path)
 
-	def save_data(self, output_path, save_attributes):
+	def save_data(self, output_path):
 		'''To read the csv file
 		'''
-		self.df[save_attributes].to_csv(output_path, index=False)
+		self.df.to_csv(output_path, index=None)
 
 	def filter_data(self, data, v_id):
 		'''To filter the dataframe for a specific vehicle
@@ -101,7 +105,7 @@ class PNEUMATrajectory:
 				*kwargs
 				):	
 		'''Using off-shelf smoothing on the specified
-			attribute series
+		attribute series
 		'''
 		if algorithm == "gaussian":
 			sm_attribute = gaussian_filter1d(self.df[attribute], window)
@@ -124,8 +128,9 @@ class PNEUMATrajectory:
 						b = 15,
 						model='xgb'):
 		'''Using a ML model to reconstruct the series of interest
-		Note that the regularization for the boosting model is depends on 
-		max value of the acceleration'''
+		Note that the regularization for the boosting model is dependent on 
+		max value of the acceleration
+		'''
 		X, y = self.df[['sv', 'a_y', 'dsv']], self.df['dsv']    
 		train_X, _, train_y, _ = train_test_split(X, y, 
 							test_size = 0.2, random_state = 123) 
@@ -143,24 +148,32 @@ class PNEUMATrajectory:
 			regr.fit(train_X, train_y)
 			return regr.predict(X)
 
-	def plot_process(self, image_path):
+	def plot_process(self, image_path, anomaly_index):
 		'''to plot the speeds and accelerations before and after treatment
 		'''
-		fig, ax = plt.subplots(2, 2, figsize=(12,6))
+		fig, ax = plt.subplots(2, 2, figsize=(14,6))
 		ax[0,0].plot(self.df.frame, self.df.dv, label="Original", linestyle= "-", color='b', alpha=0.6)
-		ax[0,0].plot(self.df.frame, self.df.dsv, label="SF filter with anomalies", color='k')
+		ax[0,0].plot(self.df.frame, self.df.dsv, label="SGF with anomalies", color='k')
 		ax[0,1].plot(self.df.frame, self.df.a_x, label="Original", linestyle= "-", alpha=0.6)
 		ax[0,1].plot(self.df[np.abs(self.df.xgb-self.df.dsv)>tol].frame, 
 					 self.df[np.abs(self.df.xgb-self.df.dsv)>tol].dsv, 'rx', label="Anomalies")
 		ax[0,1].plot(self.df.frame, self.df.xgb, label= 'Anomaly Mask', color='k')
-		ax[1,0].plot(self.df.frame, self.df.v, label='Original', alpha=0.6)
-		ax[1,0].plot(self.df.frame, self.df.g_s, label='GF with anomalies', color='r')
-		ax[1,0].plot(self.df.frame, self.df.g_ns, label='GF without anomalies', color='k')
 
-		ax[1,1].plot(self.df.frame, self.df.dv, label='Original', alpha=0.6)
-		ax[1,1].plot(self.df.frame, self.df.gdv, label='GF with anomalies', color='r')
-		# ax[1,0].plot(vehicle.df.frame, vehicle.df.sg_ns, label='SG-F without anomalies')
-		ax[1,1].plot(self.df.frame, self.df.g_na, label='GF without anomalies', color='k')   
+		if len(anomaly_index)!=0:
+			ax[1,0].plot(self.df.frame, self.df.v, label='Original', alpha=0.6)
+			ax[1,0].plot(self.df.frame, self.df.g_s, label='GF with anomalies', color='r')
+			ax[1,0].plot(self.df.frame, self.df.g_ns, label='GF without anomalies', color='k')
+
+			ax[1,1].plot(self.df.frame, self.df.dv, label='Original', alpha=0.6)
+			ax[1,1].plot(self.df.frame, self.df.gdv, label='GF with anomalies', color='r')
+			ax[1,1].plot(self.df.frame, self.df.g_na, label='GF without anomalies', color='k')   
+
+		else:
+			ax[1,0].plot(self.df.frame, self.df.v, label='Original', alpha=0.6)
+			ax[1,0].plot(self.df.frame, self.df.sv, label="SGF", color='k')
+
+			ax[1,1].plot(self.df.frame, self.df.dv, label='Original', alpha=0.6)
+			ax[1,1].plot(self.df.frame, self.df.dsv, label="SGF", color='k')
 
 		ax[0,0].set_ylabel("Acceleration ($m/s^2$)")
 		ax[0,1].set_ylabel("Acceleration ($m/s^2$)")
@@ -173,10 +186,10 @@ class PNEUMATrajectory:
 		ax[0,0].set_xticklabels([])
 		ax[0,1].set_xticklabels([])
 
-		ax[0,0].legend()
-		ax[0,1].legend()
-		ax[1,0].legend()
-		ax[1,1].legend()
+		ax[0,0].legend(loc="upper right", bbox_to_anchor=(1,1.05),handletextpad=0.1,prop={'size': 14},labelspacing = .2)
+		ax[0,1].legend(loc="upper left", bbox_to_anchor=(0,1.05),handletextpad=0.1,prop={'size': 14},labelspacing = .2)
+		ax[1,0].legend(loc="upper left", bbox_to_anchor=(0,1.05),handletextpad=0.1,prop={'size': 14},labelspacing = .2)	
+		ax[1,1].legend(loc="upper left", bbox_to_anchor=(0,1.05),handletextpad=0.1,prop={'size': 14},labelspacing = .2)
 		# place a text box in upper left in axes coords
 		props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 		ax[0,0].text(0.35, 1.05, "1. Denoise", transform=ax[0,0].transAxes, fontsize=14,
@@ -184,116 +197,149 @@ class PNEUMATrajectory:
 		ax[0,1].text(0.35, 1.05, "2. Remove anomalies", transform=ax[0,1].transAxes, fontsize=14,
 			verticalalignment='top', bbox=props)
 		ax[1,0].text(0.35, 1.05, "3. Retrieve consistent speeds", transform=ax[1,0].transAxes, fontsize=14,
-			verticalalignment='top', bbox=props)
+			verticalalignment='top', bbox=props, zorder=10)
 		ax[1,1].text(0.35, 1.05, "4. Re-calculate accelerations", transform=ax[1,1].transAxes, fontsize=14,
-			verticalalignment='top', bbox=props)
+			verticalalignment='top', bbox=props, zorder=10)
 		xmin,xmax = np.min(self.df[np.abs(self.df.xgb-self.df.dsv)>tol].frame-50),\
 				  np.max(self.df[np.abs(self.df.xgb-self.df.dsv)>tol].frame+50)
-		ax[0,0].set_xlim([xmin, xmax])
-		ax[0,1].set_xlim([xmin, xmax])
-		ax[1,0].set_xlim([xmin, xmax])
-		ax[1,1].set_xlim([xmin, xmax])
+		# ax[0,0].set_xlim([xmin, xmax])
+		# ax[0,1].set_xlim([xmin, xmax])
+		# ax[1,0].set_xlim([xmin, xmax])
+		# ax[1,1].set_xlim([xmin, xmax])
 		plt.suptitle("Vehicle id: %d"%int(self.df.id.unique()[0]))
 		plt.tight_layout()
 		plt.savefig(image_path+str(self.df.id.unique()[0])+".png", dpi=400)
-		plt.close()
-
+		fig.clear()
+		plt.close(fig)
 
 if __name__ == "__main__":
 	
-	INPUT_DATA="../data/processed/trajectory_1000.csv"
+	INPUT_DATA = 	"../data/sample_data.csv"
+	OUTPUT_PLOTS = 	"../plots/"
+	OUTPUT_DATA = 	"../data/derived/"
+	DERIVED_DATA = 	"../data/derived/"
 	
 	## define parameters of the processing
-	smoothing_window = 25 # smoothing window 
-	tol = .1 # to check the reconstruction loss in the acceleration subsequence
-	merge_subsequences = 10 # to merge subsequences if they are within these many frames apart
+	smoothing_window = 25 			# smoothing window 
+	smoothing_window_gaussian = 12
+	tol = .1 						# to check the reconstruction loss in the acceleration subsequence
+	merge_subsequences = 10 		# to merge subsequences if they are within these many frames apart
 
 	
 	data = pd.read_csv(INPUT_DATA)
-	data['frame'] = data['frame'].str.rstrip(';').astype(float)
+	# data['frame'] = data['frame'].str.rstrip(';').astype(float)
 	all_ids = data.id.unique()
 	
-	v_id = 8440
-	vehicle = PNEUMATrajectory("dummy")
-	vehicle.filter_data(data, v_id)
-	try:
-		vehicle.setter('dv', vehicle.get_derivative('v')/3.6)
-	except Exception as e:
-		print(e)
-	
-	# apply smoothing filter to the speed
-	vehicle.setter('sv', vehicle.smoother('v', window=smoothing_window, algorithm='sg'))
+	all_ids = sorted(all_ids, reverse=True)
+	print(len(all_ids))
 
-	# convert Positions from LAT-LON to UTM
-	vehicle.setter('northing', vehicle.df.apply(lambda x: np.round(utm.from_latlon(x['lat'], x['lon'])[1],8), axis=1))
-	vehicle.setter('easting', vehicle.df.apply(lambda x: np.round(utm.from_latlon(x['lat'], x['lon'])[0],8), axis=1))
-
-	# calculate the speed in KM/H
-	try:
-		vehicle.df['pos_v'] = np.round(3.6*np.sqrt(vehicle.get_derivative('northing')**2 + vehicle.get_derivative('easting')**2),4)
-		vehicle.setter('dsv', vehicle.get_derivative('sv')/3.6)
-	except Exception as e:
-		# when the array is too small for a derivative
-		print(e)
-
-	try:
-		# get the reconstructed series
-		vehicle.setter('xgb', vehicle.detect_anomalies(b=15))
-
-		# check reconstruction loss with respect to the tolerance param
-		vehicle.setter('filtered_acc', vehicle.df.apply(lambda x: x['dsv'] if np.abs(x.dsv-x.xgb)<tol else x.xgb, axis=1))
-
-		# Calculate speeds consistent with the new speeds
-		t_acc = np.array(vehicle.getter('filtered_acc')) 
-		t_speed = list(np.array(vehicle.getter('sv'))/3.6)
-		ns = t_speed[0] + 2 * 0.04* np.c_[np.r_[0, t_acc[1:-1:2].cumsum()], 
-										t_acc[::2].cumsum() - t_acc[0] / 2].ravel()[:len(t_acc)]
-		vehicle.setter('filtered_speed', ns*3.6)
-
-		# assign anomalies based on the reconstructed error	
-		vehicle.setter('anomaly', vehicle.df.apply(lambda x: 0 if np.abs(x.dsv-x.xgb)<tol else 1, axis=1))
-		ano_index=  list(vehicle.df[vehicle.df.anomaly==1].index)
-		new_indices = []
-		# merge subsequences where anomalies are close to each other e.g., within 10 frames
-		for k, j in enumerate(ano_index):
-			if k==0:
-				continue
-			if ano_index[k]-ano_index[k-1] < merge_subsequences:
-				new_indices.extend(list(range(ano_index[k-1], ano_index[k])))
-		for j in new_indices:
-			vehicle.df.loc[j, 'anomaly']=1
-
-		# replace old speeds during anomalous subsequences with the new speeds
-		vehicle.setter('new_speed', vehicle.df.apply(lambda x: x['v'] if x.anomaly==0 else x.filtered_speed, axis=1))
-
-		# apply gaussian filter on the old accelerations
-		vehicle.setter('gdv', vehicle.smoother('dv', smoothing_window))
-
-		# apply SG filter on the new speeds
-		vehicle.setter('sg_ns', vehicle.smoother('new_speed', smoothing_window, algorithm="sg"))
-
-		# apply Gaussian filter on the old speeds
-		vehicle.setter('g_s', np.round(vehicle.smoother('v', smoothing_window), 4))
-
-		# apply Gaussian filter on the new speeds
-		vehicle.setter('g_ns', vehicle.smoother('new_speed', smoothing_window))
-
-		# Calculate new accelerations from smoothed new speeds
-		vehicle.setter('sg_na', vehicle.get_derivative('sg_ns')/3.6)
-		vehicle.setter('g_na', np.round(vehicle.get_derivative('g_ns')/3.6, 4))
-
-		vehicle.plot_process("../plots/")
-		vehicle.df.rename(columns={"g_s": 'final_smoothed_speed', "g_na": "final_smoothed_acceleration"}, inplace=True)
-
-		vehicle.save_data("../data/exp"+"_"+str(vehicle.df.id.unique())+".csv", 
-						['id', 'frame', 'lon', 'lat', 'v', 'a_x', 'a_y', 'new_speed', 
-						'final_smoothed_speed', 'final_smoothed_acceleration'])
-
+	for v_id in tqdm(list(all_ids)):
+		vehicle = PNEUMATrajectory("dummy")
+		vehicle.filter_data(data, v_id)
+		try:
+			vehicle.setter('dv', vehicle.get_derivative('v')/3.6)
+		except Exception as e:
+			print(e)
+			pass
+			# raise(e)
 		
+		# apply smoothing filter to the speed
+		vehicle.setter('sv', vehicle.smoother('v', window=smoothing_window, algorithm='sg'))
 
-	except ValueError:
-		print("Vehicle not present %d" %v_id)
-		pass
-	except Exception as e:
-		raise e
-		pass
+		# convert Positions from LAT-LON to UTM
+		vehicle.setter('northing', vehicle.df.apply(lambda x: np.round(utm.from_latlon(x['lat'], x['lon'])[1],8), axis=1))
+		vehicle.setter('easting', vehicle.df.apply(lambda x: np.round(utm.from_latlon(x['lat'], x['lon'])[0],8), axis=1))
+
+		# calculate the speed in Km/h
+		try:
+			vehicle.df['pos_v'] = np.round(3.6*np.sqrt(vehicle.get_derivative('northing')**2 + vehicle.get_derivative('easting')**2),4)
+			vehicle.setter('dsv', vehicle.get_derivative('sv')/3.6)
+		except Exception as e:
+			# when the array is too small for a derivative
+			print(e)
+			pass
+			# raise(e)
+
+		try:
+			# get the reconstructed series
+			vehicle.setter('xgb', vehicle.detect_anomalies()) #default params: b=4, n=3
+
+			# check reconstruction loss with respect to the tolerance param
+			vehicle.setter('filtered_acc', vehicle.df.apply(lambda x: x['dsv'] if np.abs(x.dsv-x.xgb)<tol else x.xgb, axis=1))
+
+			# Calculate speeds consistent with the new accelerations
+			t_acc = np.array(vehicle.getter('filtered_acc')) 
+			t_speed = list(np.array(vehicle.getter('sv'))/3.6)
+			ns = t_speed[0] + 2 * 0.04* np.c_[np.r_[0, t_acc[1:-1:2].cumsum()], 
+											t_acc[::2].cumsum() - t_acc[0] / 2].ravel()[:len(t_acc)]
+			vehicle.setter('filtered_speed', ns*3.6)
+
+			# assign anomalies based on the reconstructed error	
+			vehicle.setter('anomaly', vehicle.df.apply(lambda x: 0 if np.abs(x.dsv-x.xgb)<tol else 1, axis=1))
+
+			ano_index = list(vehicle.df[vehicle.df.anomaly==1].index)
+			new_indices = []
+			# merge subsequences where anomalies are close to each other e.g., within 10 frames
+			for k, j in enumerate(ano_index):
+				if k==0:
+					continue
+				if ano_index[k]-ano_index[k-1] < merge_subsequences:
+					new_indices.extend(list(range(ano_index[k-1], ano_index[k])))
+			for j in new_indices:
+				vehicle.df.loc[j, 'anomaly']=1
+
+			if len(ano_index)!=0:
+				# if anomalies are detected, do this:
+
+				# replace old speeds during anomalous subsequences with the new speeds
+				vehicle.setter('new_speed', vehicle.df.apply(lambda x: x['v'] if x.anomaly==0 else x.filtered_speed, axis=1))
+
+				# apply gaussian filter on the old accelerations
+				vehicle.setter('gdv', vehicle.smoother('dv', smoothing_window_gaussian))
+
+				# apply SG filter on the new speeds
+				vehicle.setter('sg_ns',vehicle.smoother('new_speed', smoothing_window, algorithm="sg"))
+
+				# apply Gaussian filter on the old speeds
+				vehicle.setter('g_s', vehicle.smoother('v', smoothing_window_gaussian))
+
+				# apply Gaussian filter on the new speeds
+				vehicle.setter('g_ns', vehicle.smoother('new_speed', smoothing_window_gaussian))
+
+				# Calculate new accelerations from smoothed new speeds
+				vehicle.setter('sg_na', vehicle.get_derivative('sg_ns')/3.6)
+				vehicle.setter('g_na', vehicle.get_derivative('g_ns')/3.6)
+
+			else:
+				# else when anomalies are not detected, do this:
+
+				# replace old speeds during anomalous subsequences with the new speeds
+				vehicle.setter('new_speed', vehicle.df.apply(lambda x: x['sv'], axis=1))
+
+				vehicle.setter('gdv', vehicle.df.apply(lambda x: x['dsv'], axis=1))
+
+				vehicle.setter('sg_ns',vehicle.df.apply(lambda x: x['sv'], axis=1))
+
+				vehicle.setter('g_s', vehicle.df.apply(lambda x: x['sv'], axis=1))
+
+				vehicle.setter('g_ns', vehicle.df.apply(lambda x: x['sv'], axis=1))
+
+				vehicle.setter('sg_na', vehicle.df.apply(lambda x: x['dsv'], axis=1))
+				vehicle.setter('g_na', vehicle.df.apply(lambda x: x['dsv'], axis=1))
+
+			temp_save = pd.DataFrame(vehicle.df[["id", "frame", "g_na", "g_ns"]])
+			temp_save.columns = ["id", "frame", "g_na", "g_ns"]
+			temp_save.to_csv(OUTPUT_DATA+str(v_id)+".csv")
+			
+			vehicle.plot_process(OUTPUT_PLOTS, ano_index)
+			vehicle.save_data(OUTPUT_DATA + str(v_id)+".csv")
+
+		except ValueError as e:
+			print(e)
+			print("Vehicle not present %d" %v_id)
+			# pass
+			raise(e)
+		except Exception as e:
+			print(e)
+			pass
+			# raise(e)
